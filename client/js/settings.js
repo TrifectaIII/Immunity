ec8a50bb70c728296df85c38c3d77d7eee7ced8e
Dakota Madden-Fong <maddenfong@gmail.com>
2020-05-08 11:04:30 -0700

    added button to close settings

--- client/js/settings.js
+++ client/js/settings.js
@@ -6,22 +6,30 @@ settingsDiv = document.querySelector('.settings');
 //default is hidden
 settingsDiv.style.display = 'none';
 
+//function to toggle display of settings
+function toggleSettings () {
+    //toggle display
+    if (settingsDiv.style.display == 'none') {
+        settingsDiv.style.display = 'initial';
+    }
+    else if (
+        settingsDiv.style.display == 'initial' ||
+        settingsDiv.style.display == ''
+    ) {
+            settingsDiv.style.display = 'none';
+    }
+}
+
 //button for toggle
 settingsToggleChar = '/';
 
 //toggle display of settings when button pressed
 window.addEventListener('keypress', function (event) {
-
     //check for exact button
     if (event.keyCode == settingsToggleChar.charCodeAt(0)) {
-
-        //toggle display
-        if (settingsDiv.style.display == 'none') {
-            settingsDiv.style.display = 'initial';
-        }
-        else if (settingsDiv.style.display == 'initial' ||
-                 settingsDiv.style.display == '') {
-                    settingsDiv.style.display = 'none';
-        }
+        toggleSettings();
     }
 });
+
+//event listener for close button
+document.querySelector('.settings .settingsClose').addEventListener('click', toggleSettings);
