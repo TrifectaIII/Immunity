410d427d7396e2cdaf13a4e0b631b72e060d2718
Dakota Madden-Fong <maddenfong@gmail.com>
2020-10-29 13:18:39 -0700

    learning more re: db

--- index.js
+++ index.js
@@ -28,7 +28,17 @@ const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('stored.db');
 
+db.run("CREATE TABLE test(name text)");
 
+db.run(`INSERT INTO langs(name) VALUES(?)`, ['C'], function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+  });
+
+db.close();
 
 // HTTP SERVER
 ///////////////////////////////////////////////////
