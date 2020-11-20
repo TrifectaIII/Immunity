ece965123b562b6c10c83e92256dbff49e665f87
Dakota Madden-Fong <maddenfong@gmail.com>
2020-11-20 10:38:37 -0800

    added function to clear highscore table

--- js/Database.js
+++ js/Database.js
@@ -2,19 +2,22 @@
 const sqlite3 = require('sqlite3').verbose();
 
 //create initial DB
-function createDB () {
+function createDB (callback) {
 
     // open/create database
     let db = new sqlite3.Database('../highscore.db');
 
     //create table
-    db.run(`CREATE TABLE highscores(
-        name text,
-        score int
-    )`,
-    (err) => {
-        console.log('Failed to Create DB: Already Exists');
-    });
+    db.run(
+        `CREATE TABLE highscores(
+            name text,
+            score int
+        )`,
+        (err) => {
+            if (err) console.log('Failed to Create DB: Already Exists');
+            if (callback) callback();
+        }
+    );
 
     //close database
     db.close();
@@ -30,14 +33,10 @@ function addScore (name, score, callback) {
         "INSERT INTO highscores(name, score) VALUES(?, ?)",
         [name, score],
         function (err) {
-            if (err) {
-                throw err;
-            }
-            if (callback) {
-                callback();
-            }
+            if (err) throw err;
+            if (callback) callback();
         }
-    )
+    );
 
     //close database
     db.close();
@@ -52,12 +51,26 @@ function getScores (callback) {
         'SELECT * FROM highscores',
         [],
         (err, rows) => {
-            if (err) {
-                throw err;
-            }
-            callback(rows);
+            if (err) throw err;
+            if (callback) callback(rows);
         }
-    )
+    );
+
+    //close database
+    db.close();
+}
+
+function clearScores (callback) {
+    // open database
+    let db = new sqlite3.Database('../highscore.db');
+
+    db.run(
+        'DELETE FROM highscores',
+        (err) => {
+            if (err) throw err;
+            if (callback) callback();
+        }
+    );
 
     //close database
     db.close();
@@ -67,4 +80,5 @@ module.exports = {
     createDB,
     addScore,
     getScores,
-}   
+    clearScores,
+}
